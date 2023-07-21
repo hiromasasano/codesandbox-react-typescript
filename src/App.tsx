bf0f5cbac979562a11186085dc2f46157ca19cb6
HS <hsano@comsis.co.jp>
2023-07-21 08:41:51 +0000

    Update 5 files

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,34 @@
 import axios from "axios";
+import { useState } from "react";
+import { Todo } from "./Todo";
+import { TodoType } from "./types/todo";
+import { Text } from "./Text";
 import "./styles.css";
 
 export default function App() {
+  const [todos, setTodos] = useState<Array<TodoType>>([]);
+
   const onClickFetchData = () => {
-    axios.get("https://jsonplaceholder.typicode.com/todos").then((res) => {
-      console.log(res);
-    });
+    axios
+      .get<Array<TodoType>>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => {
+        setTodos(res.data);
+      });
   };
   return (
     <div className="App">
+      <Text color="red" fontSize="18px">
+        あああああ
+      </Text>
       <button onClick={onClickFetchData}>データ取得</button>
+      {todos.map((todo) => (
+        <Todo
+          key={todo.id}
+          title={todo.title}
+          userId={todo.userId}
+          completed={todo.completed}
+        />
+      ))}
     </div>
   );
 }
---
+++ src/Todo.tsx
@@ -0,0 +1,10 @@
+import { VFC } from "react";
+import { TodoType } from "./types/todo";
+
+export const Todo: VFC<Omit<TodoTYpe, "id">> = (
+  props: Omit<TodoType, "id">
+) => {
+  const { title, userId, completed = true } = props;
+  const completeMark = completed ? "[完]" : "[未]";
+  return <p>{`${completeMark} ${title}(ユーザ:${userId})`}</p>;
+};
